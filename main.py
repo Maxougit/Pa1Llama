7212959e3c22a5e46265fc4f83d201334acedbbb
MaxouGit <02maxou@gmail.com>
2024-04-16 07:33:02 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import chromadb
+from PyPDF2 import PdfReader
+import os
+import ollama
+
+# Initialize the ChromaDB client
+chroma_client = chromadb.Client()
+
+try:
+    collection = chroma_client.create_collection(name="pdf_docs")
+except chromadb.exceptions.UniqueConstraintError:
+    collection = chroma_client.get_collection(name="pdf_docs")
+
+def clean_text(text):
+    """Remove unnecessary characters and normalize whitespaces."""
+    return ' '.join(text.split())
+
+def chunk_text(text, chunk_size=500):
+    """Divide text into manageable chunks."""
+    words = text.split()
+    for i in range(0, len(words), chunk_size):
+        yield ' '.join(words[i:i+chunk_size])
+
+def load_and_index_pdfs(pdf_files):
+    for pdf_file in pdf_files:
+        reader = PdfReader(pdf_file)
+        full_text = ""
+        for page in reader.pages:
+            page_text = page.extract_text()
+            if page_text:
+                full_text += page_text + " "
+        full_text = clean_text(full_text)
+
+        if full_text:
+            # Generate chunks of text and index each chunk
+            for chunk in chunk_text(full_text):
+                response = ollama.embeddings(model="mxbai-embed-large", prompt=chunk)
+                embedding = response["embedding"]
+                collection.add(ids=[pdf_file], embeddings=[embedding], documents=[chunk])
+
+def retrieve_documents(prompt, n_results=3):
+    response = ollama.embeddings(model="mxbai-embed-large", prompt=prompt)
+    query_embedding = response["embedding"]
+    results = collection.query(query_embeddings=[query_embedding], n_results=n_results)
+    # print(results)
+    return results['documents']
+
+def main():
+    folder_path = "Files"
+    pdf_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".pdf")]
+
+    if pdf_files:
+        load_and_index_pdfs(pdf_files)
+        print("Indexing completed.")
+    else:
+        print("No PDF documents found for indexing.")
+
+    while True:
+        prompt = input("Posez votre question (ou tapez 'exit' pour quitter) : ")
+        if prompt.lower() == 'exit':
+            break
+        relevant_docs = retrieve_documents(prompt)
+        if relevant_docs:
+            context = relevant_docs[0]
+            response = ollama.generate(model="mistral", prompt=f"Using this data: {context} \n\n Respond to this prompt: {prompt}")
+            print("\nRéponse :", response['response'])
+        else:
+            print("\nNo relevant document initially found.")
+            response = ollama.generate(model="mistral", prompt=prompt, stream=True)
+            print("\nRéponse :", response['response'])
+        print("\n")
+
+if __name__ == "__main__":
+    main()
