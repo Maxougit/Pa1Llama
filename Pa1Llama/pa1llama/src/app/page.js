74f2ac7f20d2f11daf8a64bed44daaf27b0e0433
MaxouGit <02maxou@gmail.com>
2024-04-23 16:56:57 +0200

    better ui

--- Pa1Llama/pa1llama/src/app/page.js
+++ Pa1Llama/pa1llama/src/app/page.js
@@ -2,17 +2,29 @@
 "use client";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
-import Login from "@/components/Login";
+import Login from "@/components/login";
 import Chat from "@/components/Chat";
-
-const ChatInterface = () => (
-  <div className="h-screen flex justify-center">
-    <Chat />
-  </div>
-);
+import UploadPDF from "@/components/UploadPDF";
+import Logout from "@/components/Logout";
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showFileSystem, setShowFileSystem] = useState(false);
+
+  const toggleFileSystem = () => {
+    setShowFileSystem(!showFileSystem);
+  };
+
+  const ChatInterface = () => (
+    <div className="h-screen flex justify-center">
+      <Chat />
+      {showFileSystem && <UploadPDF />}
+      <button onClick={toggleFileSystem} className="bg-slate-600 w-20">
+        {showFileSystem ? "Hide File System" : "Show File System"}
+      </button>
+      <Logout onLogout={setIsLoggedIn} />
+    </div>
+  );
 
   useEffect(() => {
     const loggedIn = Cookies.get("loggedIn") === "true";
